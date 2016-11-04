e17a2da780f10c21152a52bdbec1560c7bd5a908
dfenjves <fenjves.daniel@gmail.com>
2016-11-04 20:06:05 +0000

    added restful routing for users and tweets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,23 +15,38 @@ class ApplicationController < Sinatra::Base
         erb :index
     end
     
-    get '/tweet' do
+    get '/tweets/new' do
         @users = User.all
-        erb :tweet
+        erb :new_tweet
     end
     
-    post '/tweets'do
+    post '/tweets/create'do
         @tweet = Tweet.create(params)
         redirect to '/'
     end
     
-    get '/user' do
-        erb :user
+    get '/tweets/:id' do
+        @tweet = Tweet.find(params[:id])
+        erb :show_tweet
     end
     
-    post '/user'do
+    get '/users/new' do
+        erb :new_user
+    end
+    
+    post '/user/create'do
         @user = User.create(params)
         redirect to '/'
+    end
+    
+    get '/users' do
+        @users = User.all
+        erb :show_users
+    end
+    
+    get '/users/:id' do
+        @user = User.find(params[:id])
+        erb :show_user
     end
 
 end
